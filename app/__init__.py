1889c8f8014ba4ccd544f3812c470e226d46bbef
System Administrator <root@admins-MacBook-Pro-10.local>
2016-08-21 17:39:11 +0400

    Removed everything Need to build separate front and backend

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from superadmin import SuperAdmin, site
-
-from django.utils.module_loading import autodiscover_modules
-
-def autodiscover():
-	print 'discovered'
-	autodiscover_modules('admin', register_to=site)
--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class InitForm(forms.Form):
-    client_id = forms.CharField(label='Client ID', max_length=140)
-    client_secret = forms.CharField(label='Client Secret', max_length=140)
-    website_url = forms.CharField(label='Website URL', max_length=140)
